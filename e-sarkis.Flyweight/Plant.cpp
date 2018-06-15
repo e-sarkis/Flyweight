ceb1eb63f3fb6e20489bae92b474aa65e7ce0e03
Elisha Sarkis <elisha.sarkis96@gmail.com>
2018-06-15 13:56:44 -0400

    Tidy up Plant cpp

--- e-sarkis.Flyweight/Plant.cpp
+++ e-sarkis.Flyweight/Plant.cpp
@@ -6,7 +6,7 @@ Plant::Plant(std::string p_speciesName,
 			 int p_waterConsumptionRate,	
 			 char p_sprite)
 	:	_speciesName(p_speciesName), 
-		_waterConsumptionRate(p_waterConsumptionRate),
+		_wateringFrequency(p_waterConsumptionRate),
 		_sprite(p_sprite)
 {}
 
