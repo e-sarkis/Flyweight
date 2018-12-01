593d135365593d75683fa7238a2238b8ac386ea9
Elisha Sarkis <elisha.sarkis96@gmail.com>
2018-12-01 12:20:37 -0500

    Add screen clearing

--- e-sarkis.Flyweight/Main.cpp
+++ e-sarkis.Flyweight/Main.cpp
@@ -6,26 +6,29 @@
 
 void main()
 {
-	std::srand(time(NULL)); // initialize random seed
+	time_t seed = time(NULL);
+	std::srand(seed); // initialize random seed
 
 	Garden garden;
-
-	std::cout << "\n" << garden.toString() << "\n";
-	std::cout << "\nTo obtain watering info of a plot:\n 1) Input X and Y coordinates separated by a single space\n 2) Press ENTER";
-	std::cout << "\n\nTo quit:\n 1)Press ESC";
 
 	int x, y;
 	while (true)
 	{
-		std::cout << "\n\nPlot: ";
-
+		system("cls");
+		// Draw GUI
+		std::cout << "Seed: " << seed << "\n";
+		std::cout << "\n" << garden.toString() << "\n";
+		std::cout << "\nTo query a plot:\n 1) Input X and Y coordinates of plot\n 2) Press ENTER";
+		std::cout << "\n\nPlot:";
 		// Obtain user input
+		std::cout << "\nX - ";
 		std::cin >> x;
+		std::cout << "Y - ";
 		std::cin >> y;
-
-		// Show plot info
+		// Draw plot info
 		std::cout << "\n" << garden.getPlotInfo(x, y);
-	}
+		system("pause");
+	} 
 
 	return;
 }
